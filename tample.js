369065517a8290a153e22ed1ff27679f7a4eebba
DenisT202 <148396944+DenisT202@users.noreply.github.com>
2024-12-26 02:08:08 +0300

    Delete tample.js

--- tample.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('commentForm').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    const name = document.getElementById('name').value;
-    const comments = document.getElementById('comments').value;
-    console.log(`Name: ${name}`);
-    console.log(`Comments: ${comments}`);
-    alert('Form submitted. Thank you for your feedback!');
-    event.target.reset();
-});
